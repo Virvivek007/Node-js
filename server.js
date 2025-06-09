4a4a47f9e15cd6bc1780d1028caa0a7a3033a741
Vivek Kumar Tiwari <157685787+Virvivek007@users.noreply.github.com>
2025-06-10 01:44:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const port = 3000
 
 app.get('/', (req, res) => {
   console.log(req.query.name)
-  res.sendFile(path.join(__dirname,'./index.html'));
-//   res.send('Hello World!')
+ 
+ res.send('Hello World! \n To see my profile change "localhost:3000/" to "localhost:3000/about"')
 })
 
 app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname,'./index.html'));
   res.send("hi, vivek kumar")
 })
 
